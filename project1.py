d9615a80c0a53aa5421ea046d870140f71a6f4fa
Mason Wong <masonjw1@uci.edu>
2023-04-21 09:40:20 -0700

    Reading input file contents function
    Made a function that reads the input file and stores its contents in a string. Also made an unittest for this function by utilizing the python standard library to make a temporary file to test the functionality of the function. Added annotations where necessary.

--- project1.py
+++ project1.py
@@ -1,6 +1,14 @@
 from pathlib import Path
 
+def read_input_file_contents(input_file) -> str:
+    """Reads the input file and stores it into a string"""
+    with open(input_file, "r") as input_file:
+        input_str = input_file.readlines()
+    return input_str
 
+# Function Not Test Covered:
+# Unable to test as the function returns user input,
+# which cannot be simulated by unittest
 def _read_input_file_path() -> Path:
     """Reads the input file path from the standard input"""
     return Path(input())
@@ -9,6 +17,8 @@ def _read_input_file_path() -> Path:
 def main() -> None:
     """Runs the simulation program in its entirety"""
     input_file_path = _read_input_file_path()
+    read_contents = read_input_file(input_file_path)
+    print(read_contents)
 
 
 if __name__ == '__main__':
--- tests/test_project1.py
+++ tests/test_project1.py
@@ -1,3 +1,19 @@
 import unittest
-
-
+import tempfile
+import os
+from pathlib import Path
+from project1 import read_input_file_contents, _read_input_file_path
+class TestProject1(unittest.TestCase):
+    """Testing all functions in project1.py"""
+    def test_read_input_file(self):
+        """Test the storing of input file into a string"""
+        input_data = "this is a test\n"
+        # This simulates the text file that is being read by the function
+        with tempfile.NamedTemporaryFile(mode = "w", delete = False) as temp_file:
+            temp_file.write(input_data)
+        output = read_input_file_contents(temp_file.name)
+        expected_output = ['this is a test\n']
+        temp_file.close()
+        # Delete the test file after unittest
+        os.remove(temp_file.name)
+        self.assertEqual(output, expected_output)
