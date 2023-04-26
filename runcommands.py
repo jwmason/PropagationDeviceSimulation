6145efe6fa397e4a876a56f5c6e2281e58ebe486
Mason Wong <masonjw1@uci.edu>
2023-04-25 23:12:41 -0700

    Storing output (runcommands.py) and sortoutput.py
    Made a new module class that handles the storage of all output from propagate functions. Also instead of printing, I store all the output into a variable and at the end, I sort by ascending number order.

--- runcommands.py
+++ runcommands.py
@@ -1,7 +1,7 @@
 """This module runs the commands after filtering and sorting"""
 
 from devices import Device
-
+from sortoutput import Output
 
 def run_device_commands(device_list, length_value) -> list:
     """Makes device objects and stores them into a list"""
@@ -26,27 +26,27 @@ def run_set_up_commands(set_up_list, device_obj_list) -> list:
     return device_obj_list
 
 
-def propagate_alert(device, receiving_device, message, sim_time, device_obj_list):
+def propagate_alert(device, receiving_device, message, sim_time, device_obj_list, output_storage):
     """Propagate the alert to the receiving device"""
     time_to_send = int(receiving_device[-1])
     # Setting current simulated time
     device.current_length = sim_time
     # Only execute if within simulated time length
-    if device.current_length < device.length:
+    if (device.current_length + time_to_send) < device.length:
         if device.device_id != int(receiving_device[1]):
-            print(f'@{sim_time}: #{device.device_id} SENT ALERT TO #{receiving_device[1]}: {message}')
-            print(f'@{sim_time + time_to_send}: #{receiving_device[1]} RECEIVED ALERT FROM #{device.device_id}: {message}')
+            output_storage.output.append(f'@{sim_time}: #{device.device_id} SENT ALERT TO #{receiving_device[1]}: {message}')
+            output_storage.output.append(f'@{sim_time + time_to_send}: #{receiving_device[1]} RECEIVED ALERT FROM #{device.device_id}: {message}')
             for next_receiving_device in device_obj_list:
                 # Check if the next receiving device is a neighbor of the current receiving device
                 if next_receiving_device.device_id == int(receiving_device[1]):
-                    return propagate_alert(device, next_receiving_device.propagate[0], message, sim_time + time_to_send, device_obj_list)
+                    return propagate_alert(next_receiving_device, next_receiving_device.propagate[0], message, sim_time + time_to_send, device_obj_list, output_storage)
         else:
             return True
     else:
         return False
 
 
-def propagate_cancel(device, receiving_device, message, sim_time, device_obj_list):
+def propagate_cancel(device, receiving_device, message, sim_time, device_obj_list, output_storage):
     """Propagate the cancel to the receiving device"""
     time_to_send = int(receiving_device[-1])
     # Setting current simulated time
@@ -54,14 +54,14 @@ def propagate_cancel(device, receiving_device, message, sim_time, device_obj_lis
     # Only execute if within simulated time length
     if device.current_length < device.length:
         if device.device_id != int(receiving_device[1]):
-            print(f'@{sim_time}: #{device.device_id} SENT CANCELLATION TO #{receiving_device[1]}: {message}')
-            print(f'@{sim_time + time_to_send}: #{receiving_device[1]} RECEIVED CANCELLATION FROM #{device.device_id}: {message}')
+            output_storage.output.append(f'@{sim_time}: #{device.device_id} SENT CANCELLATION TO #{receiving_device[1]}: {message}')
+            output_storage.output.append(f'@{sim_time + time_to_send}: #{receiving_device[1]} RECEIVED CANCELLATION FROM #{device.device_id}: {message}')
             for next_receiving_device in device_obj_list:
                 # Check if the next receiving device is a neighbor of the current receiving device
                 if next_receiving_device.device_id == int(receiving_device[1]):
-                    return propagate_cancel(device, next_receiving_device.propagate[0], message, sim_time + time_to_send, device_obj_list)
+                    return propagate_cancel(device, next_receiving_device.propagate[0], message, sim_time + time_to_send, device_obj_list, output_storage)
         else:
-            print(f'@{sim_time + time_to_send}: #{receiving_device[1]} RECEIVED CANCELLATION FROM #{receiving_device[0]}: {message}')
+            output_storage.output.append(f'@{sim_time + time_to_send}: #{receiving_device[1]} RECEIVED CANCELLATION FROM #{receiving_device[0]}: {message}')
             return True
     else:
         return False
@@ -69,6 +69,7 @@ def propagate_cancel(device, receiving_device, message, sim_time, device_obj_lis
 
 def run_command_commands(command_list, device_obj_list):
     """Runs the 'command' commands, ALERT"""
+    output_storage = Output()
     for command in command_list:
         device, message, sim_time = command.split()[1:]
         # Set the correct types for each variable
@@ -81,7 +82,8 @@ def run_command_commands(command_list, device_obj_list):
                 # Checks each propagate to see if device can send ALERT
                 if device.device_id == current_device:
                     for receiving_device in device.propagate:
-                        if propagate_alert(device, receiving_device, message, sim_time, device_obj_list):
+                        if propagate_alert(device, receiving_device, message, sim_time,
+                                           device_obj_list, output_storage):
                             alert_propagated = True
             # This is raised when the simulated time is greater than simulated length
             if alert_propagated:
@@ -93,8 +95,10 @@ def run_command_commands(command_list, device_obj_list):
                 if device.device_id == current_device:
                     for receiving_device in device.propagate:
                         if propagate_cancel(device, receiving_device, message, sim_time,
-                                           device_obj_list):
+                                           device_obj_list, output_storage):
                             cancel_propagated = True
             # This is raised when the simulated time is greater than simulated length
             if cancel_propagated:
-                print(f'@{device.length}: END')
+                output_storage.output.append(f'@{device.length}: END')
+    for output in sorted(output_storage.output, key=lambda x: int(x.split('@')[-1].split(':')[0]) if '@' in x else -1):
+        print(output)
---
+++ sortoutput.py
@@ -0,0 +1,7 @@
+"""This module is in charge of sorting output from runcommands.py"""
+
+class Output:
+    """Stores output"""
+    def __init__(self):
+        """Storing each line of output as an item"""
+        self.output = []
